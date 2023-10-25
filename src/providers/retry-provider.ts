d1a2553bc2c1444357c656aceda3ff3752dd8c4a
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2023-10-25 17:30:50 +0300

    0.5.3

--- src/providers/retry-provider.ts
+++ src/providers/retry-provider.ts
@@ -1,5 +1,5 @@
 import { retry } from '../promises'
-import { JsonRpcProvider, StaticJsonRpcProvider, JsonRpcBatchProvider } from './json-rpc-provider'
+import { JsonRpcProvider, StaticJsonRpcProvider, JsonRpcBatchProvider, StaticJsonRpcBatchProvider } from './json-rpc-provider'
 
 export class JsonRpcRetryProvider extends JsonRpcProvider {
   urls: string[]
@@ -80,6 +80,45 @@ export class JsonRpcRetryBatchProvider extends JsonRpcBatchProvider {
 }
 
 export class StaticJsonRpcRetryProvider extends StaticJsonRpcProvider {
+  urls: string[]
+  urlIndex: number = 0
+  timeouts: number[] = [5_000, 10_000, 15_000]
+  delay: number = 300
+
+  constructor (urls: string | string[], options?: { timeouts?: number[], delay?: number }) {
+    urls = Array.isArray(urls) ? urls : [urls]
+
+    super(urls[0])
+
+    this.urls = urls
+    this.urlIndex = 0
+
+    if (options && options.timeouts) {
+      this.timeouts = options.timeouts
+    }
+
+    if (options && options.delay) {
+      this.delay = options.delay
+    }
+
+    Object.setPrototypeOf(this, StaticJsonRpcRetryProvider.prototype)
+  }
+
+  public send (method: string, params: Array<any>): Promise<any> {
+    const operation = () => super.send(method, params)
+
+    return retry(operation, {
+      timeouts: this.timeouts,
+      delay: this.delay,
+      onRetry: () => {
+        this.urlIndex = (this.urlIndex + 1) % this.urls.length
+        this.connection.url = this.urls[this.urlIndex]
+      }
+    })
+  }
+}
+
+export class StaticJsonRpcRetryBatchProvider extends StaticJsonRpcBatchProvider {
   urls: string[]
   urlIndex: number = 0
   timeouts: number[] = [5_000, 10_000, 15_000]
